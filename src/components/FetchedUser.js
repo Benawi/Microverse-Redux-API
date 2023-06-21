9b9c8552ba88d8d8e5d46381cb8f264fba1d711a
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-21 15:20:07 +0300

    add the action creator to the dependency array

--- src/components/FetchedUser.js
+++ src/components/FetchedUser.js
@@ -9,4 +9,19 @@ const displayUsers=()=>{
     useEffect(()=>{
         dispatch(fetchUsers());
     },[]);
+
+    return(
+        <>
+        <h1 >List of System Users</h1>
+        {isLoading && <div>Loading ...</div>}
+        {!isLoading && error ?<div>Error...{error}</div>:null}
+        {!isLoading && users.length?(
+            <ul>
+                {
+                users.map((user)=>(<UserNames key={user.id.value} user={user}/>)
+                )}
+            </ul>
+        ): null}
+        </>
+    )
 }
