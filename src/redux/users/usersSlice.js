4430a2344e6a093760056ec7cfc067d2851b6716
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-21 14:36:58 +0300

    Import createAsyncThunk and instantiate it in a new variable

--- src/redux/users/usersSlice.js
+++ src/redux/users/usersSlice.js
@@ -1,8 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
+const url='https://randomuser.me/api/?results=20';
 const initialState ={
     users: [],
     isLoading: false,
     eror:''
 };
+
+export const fetchUsers =createAsyncThunk( 'user/fetchUsers', async()=>{
+    try{
+        return await fetch(url)
+        .then((response)=>response.json())
+    }catch(e){
+        return e;
+    }
+});
