080956885a4a2e68466ab35d9bd629ef35afe2cb
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-21 15:11:42 +0300

    Import useEffect and pass it 2 arguments: an empty function and an empty dependency array

--- src/components/FetchedUser.js
+++ src/components/FetchedUser.js
@@ -2,3 +2,11 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchUsers } from "../redux/users/usersSlice";
 import { useEffect } from "react";
 import UserNames from "./UserList";
+
+const displayUsers=()=>{
+    const {users, isLoading,error}=useSelector((state)=>state.users);
+    const dispatch=useDispatch(users);
+    useEffect(()=>{
+        dispatch(fetchUsers());
+    },[]);
+}
